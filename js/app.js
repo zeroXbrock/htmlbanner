2da7445a23e0c36e379ade26258e41dd1ba7a8f3
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-06-20 12:30:26 -0700

    default content if query param not provided

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
-const POLL_PERIOD_SECS = 30
+const POLL_PERIOD_SECS = 30;
+const DEFAULT_CONTENT_SRC = 'https://raw.githubusercontent.com/zeroXbrock/banner-content/main/content.txt';
 
 /** Register scripts to run when DOM content is loaded. */
 const init = (params) => {
     const queryParams = getQueryParams();
+    if (!('contentUrl' in queryParams)) {
+        console.warn(`No content URL provided, using default. To display your own content, set the 'contentUrl' query parameter in this page's URL.\nExample: ${window.location.href}?contentUrl=${DEFAULT_CONTENT_SRC}`)
+    }
+    const contentUrl = queryParams['contentUrl'] || DEFAULT_CONTENT_SRC
     const args = {
         ...params,
-        ...queryParams,
+        contentUrl,
     }
     document.addEventListener('DOMContentLoaded', () => {
         doInit(args)
